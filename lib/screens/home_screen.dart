caf64ffd7c6ad7198c4459fb557b99daebe6475a
Gajanand K <gajanandkkumawat95@gmail.com>
2024-04-23 11:06:12 +0530

    Worked on API Get call, Displaying the GET API Data on Screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 import 'package:todo_crud/screens/add_page_screen.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -9,12 +12,31 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  List items = [];
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    fetchTodo();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
           title: const Text("ToDo List"),
         ),
+        body: ListView.builder(
+            itemCount: items.length,
+            itemBuilder: (context, index) {
+              final item = items[index] as Map;
+              return ListTile(
+                leading: CircleAvatar(child: Text('${index + 1}')),
+                title: Text(item['title']),
+                subtitle: Text(item['description']),
+              );
+            }),
         floatingActionButton: FloatingActionButton.extended(
             onPressed: navigateToAdd, label: const Text("Add Todo")));
   }
@@ -23,5 +45,18 @@ class _HomeScreenState extends State<HomeScreen> {
     final route =
         MaterialPageRoute(builder: (context) => const AddPageScreen());
     Navigator.push(context, route);
+  }
+
+  Future<void> fetchTodo() async {
+    final url = "https://api.nstack.in/v1/todos?page=1&limit=10";
+    final uri = Uri.parse(url);
+    final response = await http.get(uri);
+    if (response.statusCode == 200) {
+      final json = jsonDecode(response.body) as Map;
+      final result = json['items'] as List;
+      setState(() {
+        items = result;
+      });
+    } else {}
   }
 }
