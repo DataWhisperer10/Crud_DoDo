462986ef32cdd55402a660881eb5f439bd56eefb
Gajanand K <gajanandkkumawat95@gmail.com>
2024-04-23 08:59:38 +0530

    Added the Home screen with floating action button to add the todo task.

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:todo_crud/screens/add_page_screen.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text("ToDo List"),
+        ),
+        floatingActionButton: FloatingActionButton.extended(
+            onPressed: () {}, label: const Text("Add Todo")));
+  }
+
+  void navigateToAdd() {
+    final route = MaterialPageRoute(builder: ((context) => AddPageScreen()));
+    Navigator.push(context, route);
+  }
+}
